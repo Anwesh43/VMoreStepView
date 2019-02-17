b04d588a4b79a4b440eefc4c4c460276c4bdff3b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 11:32:03 +0530

    adding VMoreStep class

--- vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
+++ vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
@@ -164,7 +164,31 @@ class VMoreStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class VMoreStep(var i : Int) {
+
+        private val root : VMSNode = VMSNode(0)
+        private var curr : VMSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
