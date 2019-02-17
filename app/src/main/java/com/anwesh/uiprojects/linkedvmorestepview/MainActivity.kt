a504d278f54e86be86b39e7a8a5dd41c02941828
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 11:47:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedvmorestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedvmorestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedvmorestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.vmorestepview.VMoreStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view : VMoreStepView = VMoreStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
