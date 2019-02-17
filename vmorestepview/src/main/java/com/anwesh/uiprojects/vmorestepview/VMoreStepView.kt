87be93a98daa969abb1d809f4f7b77f53bced6cc
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 10:17:33 +0530

    adding constants and importing libraries

--- vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
+++ vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.vmorestepview
 /**
  * Created by anweshmishra on 17/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#6A1B9A")
+val backColor : Int = Color.parseColor("#212121")
