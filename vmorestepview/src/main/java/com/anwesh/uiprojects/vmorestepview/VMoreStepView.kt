2b44a4d7e08cb6aad27d529634af3f154c62438f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 12:08:40 +0530

    adding fixes

--- vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
+++ vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
@@ -13,7 +13,7 @@ import android.app.Activity
 import android.content.Context
 
 val nodes : Int = 5
-val lines : Int = 3
+val lines : Int = 6
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
@@ -25,7 +25,7 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
-fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 fun Int.sfi() : Float = 1f - 2 * (this % 2)
 fun Int.sf() : Float = 1f - 2 * this
