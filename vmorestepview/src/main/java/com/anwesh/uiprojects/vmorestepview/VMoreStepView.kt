26e487ee74c9b9608a00c59b55a3d8cffdbbca26
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 11:18:19 +0530

    adding VMSNode class

--- vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
+++ vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
@@ -123,4 +123,48 @@ class VMoreStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VMSNode(var i : Int, val state : State = State()) {
+
+        private var next : VMSNode? = null
+        private var prev : VMSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = VMSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVMSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VMSNode {
+            var curr : VMSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
