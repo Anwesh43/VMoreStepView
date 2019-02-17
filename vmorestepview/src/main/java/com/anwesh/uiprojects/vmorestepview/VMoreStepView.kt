8ab5d3a58004cfe601482f0e0ff488d29004220d
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 11:03:09 +0530

    adding vmorestepview class

--- vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
+++ vmorestepview/src/main/java/com/anwesh/uiprojects/vmorestepview/VMoreStepView.kt
@@ -58,3 +58,21 @@ fun Canvas.drawVMSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class VMoreStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
